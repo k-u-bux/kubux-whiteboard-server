a7b1693c8dd5e56a76c11c44f84c2df9a425e134
Kai-Uwe Bux <bux_gitlab@kubux.net>
2025-09-13 21:48:16 +0200

    snapshots

--- server.js
+++ server.js
@@ -543,9 +543,9 @@ messageHandlers[MESSAGES.CLIENT_TO_SERVER.FULL_PAGE_REQUESTS.TYPE] = (ws, data, 
 };
 
 function handleEditAction(page, action) {
-    const current_visible = compileVisualState( page.history.slice( 0, page.present ) ).visible;
-    const lhs = serialize( page.state.visible );
-    const rhs = serialize( current_visible );
+    const current_visible = compileVisualState( page.history.slice( 0, page.present + 1 ) ).visible;
+    const rhs = serialize( page.state.visible );
+    const lhs = serialize( current_visible );
     if ( lhs != rhs ) {
         console.log("visible set BAD",`time = ${page.present}, actual = ${lhs} vs predicted = ${rhs}`);
         process.exit( 1 );
