da09787b2d72800af92d53ac7acc06638874c05f
Kai-Uwe Bux <bux_gitlab@kubux.net>
2025-09-14 19:55:23 +0200

    snapshots

--- server.js
+++ server.js
@@ -543,7 +543,7 @@ messageHandlers[MESSAGES.CLIENT_TO_SERVER.FULL_PAGE_REQUESTS.TYPE] = (ws, data, 
 };
 
 function handleEditAction(page, action) {
-    const current_visible = compileVisualState( page.history.slice( 0, page.present + 1 ) ).visible;
+    const current_visible = compileVisualState( page.history.slice( 0, page.present ) ).visible;
     // const rhs = serialize( page.state.visible );
     // const lhs = serialize( current_visible );
     // if ( lhs != rhs ) {
