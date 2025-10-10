9e332e5da74f72e4cdd40dc22776fd85df0688dd
Kai-Uwe Bux <bux_gitlab@kubux.net>
2025-10-10 06:32:21 +0200

    new icon

--- rounded_corners.bash
+++ rounded_corners.bash
@@ -1,8 +1,10 @@
-convert app-icon.png \
+convert app-icon.png -resize 512x512 dummy.png
+convert dummy.png \
   -alpha set \
   \( -size 512x512 xc:none \
     -fill white \
     -draw "roundrectangle 20,20 491,491, 75,75" \
   \) \
   -compose DstIn -composite icon-512.png
-convert output.png -resize 192x192 icon-192.png
+convert icon-512.png -resize 192x192 icon-192.png
+rm dummy.png
