536d24612c4cccc210bd7b0d8c14c4748b98f2fe
Kai-Uwe Bux <bux_gitlab@kubux.net>
2026-04-25 19:01:04 +0200

    add: favicon

--- rounded_corners.bash
+++ rounded_corners.bash
@@ -6,5 +6,13 @@ convert dummy.png \
     -draw "roundrectangle 20,20 491,491, 75,75" \
   \) \
   -compose DstIn -composite icon-512.png
+
 convert icon-512.png -resize 192x192 icon-192.png
+
+convert icon-512.png \
+  \( -clone 0 -resize 16x16 \) \
+  \( -clone 0 -resize 32x32 \) \
+  \( -clone 0 -resize 48x48 \) \
+  -delete 0 favicon.ico
+
 rm dummy.png
