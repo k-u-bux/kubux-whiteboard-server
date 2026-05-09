36043ead527c442334793b51f15e2f8c541d2d0a
Kai-Uwe Bux <bux_gitlab@kubux.net>
2026-05-09 10:19:12 +0200

    investigate dash patterns

--- shared.js
+++ shared.js
@@ -653,7 +653,7 @@ const STROKE_STYLES = {
         1.0,                  // opacity
         CAP_STYLES.ROUND,      // capStyle
         JOIN_STYLES.BEVEL,    // joinStyle
-        [1,5],                  // dashPattern
+        [5,1],                  // dashPattern
         1.0,                  // pressureSensitivity
         1,                    // layer
         PEN_TYPES.MARKER      // penType
